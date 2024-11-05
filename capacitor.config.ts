ec103a6baff1018361ae49bfcbcb315d9e030779
JayBe96 <102026829+JayBe96@users.noreply.github.com>
2024-11-05 10:43:51 +0100

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'joggin.tracker',
+  appName: 'joggin-tracker',
+  webDir: 'www'
+};
+
+export default config;
