7d6bfe3eebba37db11e28ef98b4ee3ea55f8e5d8
JayBe96 <102026829+JayBe96@users.noreply.github.com>
2024-11-12 14:09:29 +0100

    Added Tabbar with icons

--- src/app/homescreen/homescreen.page.ts
+++ src/app/homescreen/homescreen.page.ts
@@ -1,14 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonTabBar, IonTabButton, IonIcon, IonLabel, IonTabs } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { barbellOutline, statsChartOutline } from 'ionicons/icons';
+
+addIcons({
+  'barbell-outline': barbellOutline,
+  'stats-chart-outline': statsChartOutline
+});
 
 @Component({
   selector: 'app-homescreen',
   templateUrl: './homescreen.page.html',
   styleUrls: ['./homescreen.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonTabBar,
+    IonTabButton,
+    IonIcon,
+    IonLabel,
+    IonTabs,
+    CommonModule,
+    FormsModule
+  ]
 })
 export class HomescreenPage implements OnInit {
 
