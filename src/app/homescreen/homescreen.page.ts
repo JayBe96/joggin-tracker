fd7e97a4bff7d575b470ea126018511e6a89281e
JayBe96 <102026829+JayBe96@users.noreply.github.com>
2024-11-18 11:45:57 +0100

    feat: enhance homescreen with gradient background and blur effects

--- src/app/homescreen/homescreen.page.ts
+++ src/app/homescreen/homescreen.page.ts
@@ -1,7 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { 
+  IonTabButton,
+  IonIcon,
+  IonLabel,
+  IonTabs,
+  IonList,
+  IonItem,
+  IonContent
+} from '@ionic/angular/standalone';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonTabBar, IonTabButton, IonIcon, IonLabel, IonTabs, IonList, IonItem } from '@ionic/angular/standalone';
 import { TabsComponent } from '../tabs/tabs.component';
 
 @Component({
@@ -10,17 +18,13 @@ import { TabsComponent } from '../tabs/tabs.component';
   styleUrls: ['./homescreen.page.scss'],
   standalone: true,
   imports: [
-    IonContent,
-    IonHeader,
-    IonTitle,
-    IonToolbar,
-    IonTabBar,
     IonTabButton,
     IonIcon,
     IonLabel,
     IonTabs,
     IonList,
     IonItem,
+    IonContent,
     CommonModule,
     FormsModule,
     TabsComponent
@@ -29,6 +33,5 @@ import { TabsComponent } from '../tabs/tabs.component';
 export class HomescreenPage implements OnInit {
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 }
