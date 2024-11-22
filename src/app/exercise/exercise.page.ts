febbb013c451d16e6cec9bd07572f6c0c241444c
JayBe96 <102026829+JayBe96@users.noreply.github.com>
2024-11-22 11:30:53 +0100

    feat: add exercise selection and input fields for running details

--- src/app/exercise/exercise.page.ts
+++ src/app/exercise/exercise.page.ts
@@ -3,6 +3,7 @@ import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { TabsComponent } from '../tabs/tabs.component';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-exercise',
@@ -17,8 +18,16 @@ import { TabsComponent } from '../tabs/tabs.component';
   ]
 })
 export class ExercisePage implements OnInit {
+  selectedExercise: string = '';
+  runDuration: number = 0;
+  runDistance: number = 0;
+  runRating: string = '';
 
   constructor() {}
 
   ngOnInit() {}
+
+  saveRun() {
+    // Implement save functionality here
+  }
 }
