983a8f6c368b0556c888704ccf0b83cec5490914
JayBe96 <102026829+JayBe96@users.noreply.github.com>
2024-11-25 11:40:47 +0100

    feat: enhance data page with runs and meditations display, add data service for API integration

--- src/app/data/data.page.ts
+++ src/app/data/data.page.ts
@@ -1,21 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonListHeader, IonItem, IonLabel, IonCard, IonCardHeader, IonCardTitle, IonCardContent } from '@ionic/angular/standalone';
 import { TabsComponent } from '../tabs/tabs.component';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.page.html',
   styleUrls: ['./data.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, TabsComponent]
+  imports: [
+    IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, TabsComponent,
+    IonList, IonListHeader, IonItem, IonLabel, IonCard, IonCardHeader, IonCardTitle, IonCardContent
+  ]
 })
 export class DataPage implements OnInit {
+  runs: any[] = [];
+  meditations: any[] = [];
 
-  constructor() { }
+  constructor(private dataService: DataService) {}
 
   ngOnInit() {
+    this.fetchData();
   }
 
+  ionViewWillEnter() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.dataService.getRuns().subscribe(data => {
+      this.runs = data;
+    });
+
+    this.dataService.getMeditations().subscribe(data => {
+      this.meditations = data;
+    });
+  }
 }
