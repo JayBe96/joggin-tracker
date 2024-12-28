f2afef44846a1e56b7f575ec757157caf3a300fd
JayBe96 <102026829+JayBe96@users.noreply.github.com>
2024-12-28 14:12:52 +0100

    feat: enhance exercise and meditation tests with new functionality and toast notifications

--- cypress/e2e/app/exercise.cy.js
+++ cypress/e2e/app/exercise.cy.js
@@ -5,6 +5,12 @@ describe('Exercise Page Tests', () => {
     cy.url().should('include', '/tabs/exercise');
   });
 
+  afterEach(() => {
+    cy.fixture('testdatabase.json').then((data) => {
+      cy.writeFile('jsonserver/database/database.json', data);
+    });
+  });
+
   it('Switches to Meditation Segment', () => {
     cy.get('[data-testid="exercise-segment"]').within(() => {
       cy.get('[data-testid="segment-meditation"]').click();
@@ -19,9 +25,9 @@ describe('Exercise Page Tests', () => {
     cy.get('[data-testid="run-section"]').should('be.visible');
 
     // Enter Run Details
-    cy.get('[data-testid="input-runName"]').type('Test Run');
-    cy.get('[data-testid="input-runDuration"]').type('30');
-    cy.get('[data-testid="input-runDistance"]').type('5');
+    cy.get('[data-testid="input-runName"] input').clear().type('Test Run');
+    cy.get('[data-testid="input-runDuration"] input').clear().type('30');
+    cy.get('[data-testid="input-runDistance"] input').clear().type('5');
 
     // Interact with ion-select for Run Rating
     cy.get('[data-testid="select-runRating"]').click();
@@ -35,13 +41,103 @@ describe('Exercise Page Tests', () => {
     cy.get('[data-testid="button-saveRun"]').click();
 
     // Verify the Run is Saved in the Database
-    cy.readFile('/Users/jonas/Documents/GitHub/Bachelorthesis/joggin-tracker/jsonserver/database/database.json').then((data) => { // TODO: fixed path
+    cy.readFile('jsonserver/database/database.json').then((data) => { // TODO: fixed path
       const runs = data.runs;
       const newRun = runs.find(run => run.name === 'Test Run');
       expect(newRun).to.exist;
       expect(newRun.duration).to.equal(30);
       expect(newRun.distance).to.equal(5);
       expect(newRun.rating).to.equal('good');
+    });
+  });
+
+  
+  it('Should Save a New Run (Toast)', () => {
+    // Switch to Run Segment
+    cy.get('[data-testid="segment-run"]').click();
+    cy.get('[data-testid="run-section"]').should('be.visible');
+
+    // Enter Run Details
+    cy.get('[data-testid="input-runName"] input').clear().type('Test Run');
+    cy.get('[data-testid="input-runDuration"] input').clear().type('30');
+    cy.get('[data-testid="input-runDistance"] input').clear().type('5');
+
+    // Interact with ion-select for Run Rating
+    cy.get('[data-testid="select-runRating"]').click();
+
+    // Handle ion-alert Overlay
+    cy.get('ion-alert').should('be.visible');
+    cy.get('ion-alert').find('button').contains('😊 Good').click();
+    cy.get('ion-alert').find('button').contains('OK').click({ force: true });
+
+    // Save Run
+    cy.get('[data-testid="button-saveRun"]').click();
+
+    // Verify the Toast is Displayed and Green
+    cy.get('ion-toast', { timeout: 10000 })
+    .should('have.attr', 'color', 'success');
+
+  });
+
+  it('Should Save a New Meditation and show Toast', () => {
+    // Switch to Meditation Segment
+    cy.get('[data-testid="exercise-segment"]').within(() => {
+      cy.get('[data-testid="segment-meditation"]').click();
+    });
+    cy.get('[data-testid="meditation-section"]').should('be.visible');
+
+    // Enter Meditation Details
+    cy.get('[data-testid="input-meditationName"] input').clear().type('Test Meditation');
+    cy.get('[data-testid="input-meditationDuration"] input').clear().type('15');
+    cy.get('[data-testid="radio-meditationType"] ion-radio[value="Mindfulness meditation"]').click();
+
+    // Interact with ion-select for Meditation Rating
+    cy.get('[data-testid="select-meditationRating"]').click();
+
+    // Handle ion-alert Overlay
+    cy.get('ion-alert').should('be.visible');
+    cy.get('ion-alert').find('button').contains('😊 Good').click();
+    cy.get('ion-alert').find('button').contains('OK').click({ force: true });
+
+    // Save Meditation
+    cy.get('[data-testid="button-saveMeditation"]').click();
+
+    // Verify the Toast is Displayed and Green
+    cy.get('ion-toast', { timeout: 10000 })
+    .should('have.attr', 'color', 'success');
+  });
+
+  it('Should Save a New Meditation and store in Database', () => {
+    // Switch to Meditation Segment
+    cy.get('[data-testid="exercise-segment"]').within(() => {
+      cy.get('[data-testid="segment-meditation"]').click();
+    });
+    cy.get('[data-testid="meditation-section"]').should('be.visible');
+
+    // Enter Meditation Details
+    cy.get('[data-testid="input-meditationName"] input').clear().type('Test Meditation');
+    cy.get('[data-testid="input-meditationDuration"] input').clear().type('15');
+    cy.get('[data-testid="radio-meditationType"] ion-radio[value="Mindfulness meditation"]').click();
+    
+    // Interact with ion-select for Meditation Rating
+    cy.get('[data-testid="select-meditationRating"]').click();
+
+    // Handle ion-alert Overlay
+    cy.get('ion-alert').should('be.visible');
+    cy.get('ion-alert').find('button').contains('😊 Good').click();
+    cy.get('ion-alert').find('button').contains('OK').click({ force: true });
+
+    // Save Meditation
+    cy.get('[data-testid="button-saveMeditation"]').click();
+
+    // Verify the Meditation is Saved in the Database
+    cy.readFile('jsonserver/database/database.json').then((data) => {
+      const meditations = data.meditations;
+      const newMeditation = meditations.find(meditation => meditation.name === 'Test Meditation');
+      expect(newMeditation).to.exist;
+      expect(newMeditation.duration).to.equal(15);
+      expect(newMeditation.type).to.equal('Mindfulness meditation');
+      expect(newMeditation.rating).to.equal('good');
     });
   });
 
