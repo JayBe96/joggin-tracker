e77bad30e695d81151b859bf79df319a76e491b7
JayBe96 <102026829+JayBe96@users.noreply.github.com>
2025-01-09 11:24:35 +0100

    refactor: remove unnecessary comments and simplify the LoginPage class structure

--- test/pageobjects/login.page.ts
+++ test/pageobjects/login.page.ts
@@ -1,13 +1,7 @@
 import { $ } from '@wdio/globals'
 import Page from './page';
 
-/**
- * sub page containing specific selectors and methods for a specific page
- */
 class LoginPage extends Page {
-    /**
-     * define selectors using getter methods
-     */
     public get inputUsername() {
         return $('ion-input[name="username"]');
     }
@@ -20,19 +14,12 @@ class LoginPage extends Page {
         return $('ion-button[type="submit"]');
     }
 
-    /**
-     * method to login with given username and password
-     */
     public async login (username: string, password: string) {
         await this.inputUsername.$('input').setValue(username);
         await this.inputPassword.$('input').setValue(password);
-
         await this.btnSubmit.click();
     }
 
-    /**
-     * overwrite specific options to adapt it to page object
-     */
     public override open () {
         return super.open('login');
     }
